86a71c86f2bcbe3bffb4a68aa0fa6e390af68afe
teanteagame <teanteagame@gmail.com>
2026-05-13 09:49:50 +0700

    Hoàn thành đợt tân trang thứ nhất

--- src/main/java/config/DatabaseConnect.java
+++ src/main/java/config/DatabaseConnect.java
@@ -5,30 +5,29 @@ import java.sql.DriverManager;
 
 public class DatabaseConnect {
 
+    // Thông tin kết nối trực tiếp tới Aiven
     private static final String URL =
-            "jdbc:mysql://localhost:3306/hotel_management";
+            "jdbc:mysql://qlks-teanteagame.h.aivencloud.com:22331/hotel_management?ssl-mode=REQUIRED";
 
-    private static final String USER = "root";
+    private static final String USER = "avnadmin";
 
-    private static final String PASSWORD = "D05022006d";
+    private static final String PASSWORD = "AVNS_1ixtKWgnlSnULFmvqNW";
 
     public static Connection getConnection() {
-
         try {
-
+            // Nạp Driver MySQL
             Class.forName("com.mysql.cj.jdbc.Driver");
 
+            // Thiết lập kết nối
             return DriverManager.getConnection(
                     URL,
                     USER,
                     PASSWORD
             );
-
         } catch (Exception e) {
-
+            System.err.println("❌ Lỗi kết nối server Aiven: " + e.getMessage());
             e.printStackTrace();
         }
-
         return null;
     }
 }
